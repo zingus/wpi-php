e959e1e5ba102b2b48fd0ab384a2ca52b001ca39
Zingus Rinkle <zingus@gmail.com>
2014-02-13 05:52:05 +0100

    svg generation albeit borken

--- wpi.php
+++ wpi.php
@@ -57,8 +57,9 @@ class wpi extends bindata
 }
 
 /*main*/
-$parser=new wpi('SKETCH11.WPI');
+$parser=new wpi('SKETCH5.WPI');
 $drawing=$parser->process();
+file_put_contents('dump.txt',"".$drawing);
 $renderer=new wpiRenderer($drawing);
 $renderer->toSVG('some.svg');
 
@@ -66,49 +67,60 @@ class wpiLayer
 {
   function wpiLayer()
   {
-    $this->strokes=array();
+    $this->strokes=array(new wpiStroke());
     $this->idx=0;
   }
 
   function startStroke()
   {
-    $this->strokes[]=new wpiStroke();
-    $this->idx++;
+    $stroke=$this->currentStroke();
+    if(!$stroke->isEmpty())
+      $this->addStroke();
   }
 
   function endStroke()
   {
-    $this->strokes[]=array('E');
+    //$this->addStroke();
+  }
+
+  function addStroke()
+  {
+    $this->idx++;
+    $this->strokes[$this->idx]=new wpiStroke();
   }
 
   function addPoint($x,$y)
   {
-    $this->strokes[]=array('x',$x,$y);
+    $stroke=$this->currentStroke();
+    $stroke->addPoint($x,$y);
   }
 
   function addPressure($pressure)
   {
-    $this->strokes[]=array('p',$pressure);
+    $stroke=$this->currentStroke();
+    $stroke->addPressure($pressure);
   }
 
-  function addTilt($pressure)
+  function addTilt($tilt)
   {
-    $this->strokes[]=array('t',$pressure);
+    $stroke=$this->currentStroke();
+    $stroke->addTilt($tilt);
   }
 
   function __tostring()
   {
     $ret='';
-    foreach($this->strokes as $v)
+    foreach($this->strokes as $stroke)
     {
-      $ret.=implode(' ',$v)."\n";
+      //$ret.=implode(' ',$v)."\n";
+      $ret.=$stroke->__tostring();
     }
     return $ret;
   }
 
   function currentStroke()
   {
-    return $this->stroke[$this->idx];
+    return $this->strokes[$this->idx];
   }
 }
 
@@ -120,6 +132,11 @@ class wpiStroke
     $this->coordY   = array();
     $this->pressure = array();
     $this->tilt     = array();
+  }
+
+  function isEmpty()
+  {
+    return count($this->coordX)===0;
   }
 
   function addPoint($x,$y)
@@ -136,6 +153,16 @@ class wpiStroke
   function addTilt($tilt)
   {
     $this->tilt[]=$tilt;
+  }
+
+  function __tostring()
+  {
+    $ret="<stroke>\n";
+    foreach($this->coordX as $k=>$x) {
+      $ret.=sprintf("  x %3d y %3d p %3d t %3d\n",$x,$this->coordY[$k],$this->pressure[$k],$this->tilt[$k]);
+    }
+    $ret.="</stroke>\n";
+    return $ret;
   }
 }
 
@@ -193,7 +220,7 @@ class wpiDrawing
     $ret='';
     foreach($this->layers as $layer)
     {
-      $ret.=$layer->__tostring();
+      $ret.="<layer>\n  ".trim(strtr($layer->__tostring(),array("\n"=>"\n  ")))."\n</layer>\n";
     }
     return $ret;
   }
@@ -220,15 +247,33 @@ class wpiRenderer
 
   function toSVG($filename='')
   {
+    $drawing=$this->drawing;
     list($x1,$y1,$x2,$y2)=$this->findRange();
     $w=$x2-$x1; $W=$w+10; $Wcm='21cm';
     $h=$y2-$y1; $H=$h+10; $Hcm=(21*$h/$w).'cm';
-    $d="M 100 100 L 300 100 L 200 300 z"; // TODO actual rendering
+    $paths='';
+    foreach($drawing->strokes() as $stroke) {
+      $d='';
+      foreach($stroke->coordX as $k=>$x) {
+        $y=$stroke->coordY[$k];
+        if($k==0)
+          $d.="M $x $y S $x $y ";
+        // normalize coords
+        $x-=$x1;
+        $y-=$y1;
+        // append to path data
+        //$d.="$cmd $x $y ";
+        $d.="$x $y ";
+      }
+      $d.='z';
+      $color='#'.substr(md5($k),0,6);
+      $paths.="<path d='$d' fill='none' stroke='$color' stroke-width='1' />\n";
+    }
     $ret="<?xml version='1.0' standalone='no'?>
 <!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'>
 <svg width='$Wcm' height='$Hcm' viewBox='0 0 $W $H' xmlns='http://www.w3.org/2000/svg' version='1.1'><title>{$this->title}</title><desc>{$this->desc}</desc>
 <rect x='5' y='5' width='$w' height='$h' fill='none' stroke='blue' />
-<path d='$d' fill='red' stroke='blue' stroke-width='3' />
+$paths
 </svg>";
   
 
@@ -243,15 +288,15 @@ class wpiRenderer
     $d=$this->drawing;
     $minX=$minY=null;
     $maxX=$maxY=null;
-    foreach($d->strokes() as $v) {
-      @list($t,$x,$y)=$v;
-      switch($t){
-        case 'x':
-          if($minX===null) $minX=$maxX=$x;
-          if($minY===null) $minY=$maxY=$y;
+    foreach($d->strokes() as $stroke) {
+      if($minX===null) $minX=$maxX=$stroke->coordX[0];
+      if($minY===null) $minY=$maxY=$stroke->coordY[0];
+      foreach($stroke->coordX as $x) {
           if($x>$maxX) $maxX=$x;
-          if($y>$maxY) $maxY=$y;
           if($x<$minX) $minX=$x;
+      }
+      foreach($stroke->coordY as $y) {
+          if($y>$maxY) $maxY=$y;
           if($y<$minY) $minY=$y;
       }
     }
